94bd278d9f7db7bda9eecc6431df0100843d8630
harichopper <haricdonh@gmail.com>
2024-12-10 19:34:17 +0530

    project completd

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5001", // Adjust based on your frontend port
+    origin: "http://localhost:5173", // Adjust based on your frontend port
     credentials: true,
   })
 );
