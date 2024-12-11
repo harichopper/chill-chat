3c3ab142b0e28679dae8bf80fe939088a33a3684
harichopper <haricdonh@gmail.com>
2024-12-11 15:28:19 +0530

    project completd

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const BASE_URL = import.meta.env.MODE === "production" ? "http://localhost:5001" : "https://chill-chat-9do2.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
