870a44c5a06db26bdc3110ecd79d348971dce7ae
harichopper <haricdonh@gmail.com>
2024-12-11 20:07:26 +0530

    project completd

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development"
+const BASE_URL = import.meta.env.MODE === "production"
   ? "http://localhost:5001" 
   : "https://chill-chat-9do2.onrender.com"; // Use your deployed backend URL
 
