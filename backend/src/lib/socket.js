dda3f47348c1a0744e8527d409acc83aa262dcf6
harichopper <haricdonh@gmail.com>
2024-12-11 16:41:09 +0530

    project completd

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: process.env.NODE_ENV === 'production' ? "https://chill-chat-9do2.onrender.com" : "http://localhost:5173",
   },
 });
 
@@ -17,6 +17,13 @@ export function getReceiverSocketId(userId) {
 
 // used to store online users
 const userSocketMap = {}; // {userId: socketId}
+function sendMessageToUser(receiverId, message) {
+  const socketId = userSocketMap[receiverId];
+  if (socketId) {
+    io.to(socketId).emit("message", message);
+  }
+}
+
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
@@ -33,5 +40,19 @@ io.on("connection", (socket) => {
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
+const socket = io("http://localhost:5001", {
+  query: {
+    token: localStorage.getItem("token"), // or however you store the token
+  },
+});
+
+socket.on("getOnlineUsers", (userIds) => {
+  console.log("Online users:", userIds);
+});
+
+socket.on("newMessage", (message) => {
+  console.log("New message:", message);
+});
+
 
 export { io, app, server };
